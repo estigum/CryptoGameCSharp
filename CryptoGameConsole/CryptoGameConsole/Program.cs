f9563d9212bea936346d05dec937708746cdc386
estigum <estigum@gmail.com>
2022-04-08 21:56:14 -0400

    Added a new comment

--- CryptoGameConsole/CryptoGameConsole/Program.cs
+++ CryptoGameConsole/CryptoGameConsole/Program.cs
@@ -12,6 +12,7 @@ namespace CryptoGameConsole
         {
             Console.WriteLine("Crypto Game. Test application");
             Console.WriteLine("Will add more to this application");
+            Console.WriteLine("Add a branch!!");
             Console.ReadKey();
 
         }
